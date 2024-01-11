7e125b19b7d07063d8d653eb2c3946b02e7b9aeb
leechangjoon <2018110021@dongguk.edu>
2024-01-11 20:34:54 +0900

    [Degisn] footer 팀 이름 수정...

--- src/pages/layout/Footer.jsx
+++ src/pages/layout/Footer.jsx
@@ -3,7 +3,7 @@ import * as S from "./style";
 const Footer = () => {
   return (
     <div>
-      <span>동국톤 7팀 SnowPick입니다.</span>
+      <span>눈꽃톤 7팀 SnowPick입니다.</span>
       <ol>
         <li>김교휘</li>
         <li>이창준</li>
