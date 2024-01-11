e4367aa07fff677835ba3f78968e7fa93cad4249
leechangjoon <2018110021@dongguk.edu>
2024-01-12 03:47:15 +0900

    [Design] Nav 마우스 호버, 클릭 UI 추가

--- src/pages/layout/Navbar.jsx
+++ src/pages/layout/Navbar.jsx
@@ -1,17 +1,13 @@
+// Navbar.jsx
 import * as S from "./style";
-import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <S.NavContainer>
       <h1>상단 네브바</h1>
       <S.NavItemContainer>
-        <NavLink to="/">
-          <S.NavItem>Home</S.NavItem>
-        </NavLink>
-        <NavLink to="/list">
-          <S.NavItem>ImageList</S.NavItem>
-        </NavLink>
+        <S.StyledNavLink to="/">이미지 만들기</S.StyledNavLink>
+        <S.StyledNavLink to="/list">구경하기</S.StyledNavLink>
       </S.NavItemContainer>
     </S.NavContainer>
   );
--- src/pages/layout/style.jsx
+++ src/pages/layout/style.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const NavContainer = styled.div`
   height: 75px;
@@ -15,8 +16,34 @@ export const NavItemContainer = styled.nav`
   margin-right: 30px;
 `;
 
+export const StyledNavLink = styled(NavLink)`
+  margin-right: 30px;
+  text-decoration: none; // 밑줄 제거
+  color: inherit; // 부모 요소의 색상 상속
+
+  &:hover {
+    text-decoration: none; // 마우스 오버 시 밑줄 제거
+    color: blue; // 마우스 오버 시 색상 유지
+  }
+
+  &.active {
+    color: black; // 활성화된 링크의 색상 유지
+  }
+`;
+
 export const NavItem = styled.span`
   margin-right: 30px;
+  text-decoration: none; // 밑줄 제거
+  color: inherit; // 부모 요소의 색상 상속
+
+  &:hover {
+    text-decoration: none; // 마우스 오버 시 밑줄 제거
+    color: inherit; // 마우스 오버 시 색상 유지
+  }
+
+  &.active {
+    color: inherit; // 활성화된 링크의 색상 유지
+  }
 `;
 
 export const FooterContainer = styled.div`
