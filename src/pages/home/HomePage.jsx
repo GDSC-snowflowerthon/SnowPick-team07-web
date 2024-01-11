671f9ef726f48e1354b23f19c99d6cf69a458911
leechangjoon <2018110021@dongguk.edu>
2024-01-11 17:39:50 +0900

    [Design] favicon 추가

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,8 @@
 import SnowEffectOnUploadedImage from "../../snowEffect";
-import NavBar from "../layout/Navbar";
 
 const HomePage = () => {
   return (
     <div>
-      {/* <NavBar /> */}
       <SnowEffectOnUploadedImage />
     </div>
   );
---
+++ src/pages/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+const Footer = () => {
+  return (
+    <div>
+      <span>Footer 내용입니다. 심심해서 만들어봄</span>
+    </div>
+  );
+};
+
+export default Footer;
