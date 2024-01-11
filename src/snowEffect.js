fd72f4900b776ddf0a9862a4c4336c4262b00593
leechangjoon <2018110021@dongguk.edu>
2024-01-12 01:14:10 +0900

    [Feat] GIF 다운로드 기능 추가

--- src/snowEffect.js
+++ src/snowEffect.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
+import GIF from "gif.js";
 
 const SnowEffectOnUploadedImage = () => {
   const canvasRef = useRef(null);
@@ -95,6 +96,56 @@ const SnowEffectOnUploadedImage = () => {
     };
   }
 
+  const generateGIF = () => {
+    if (!canvasRef.current || !image) return;
+
+    const gif = new GIF({
+      workers: 2,
+      quality: 10,
+      width: imageSize.width,
+      height: imageSize.height,
+    });
+
+    const ctx = canvasRef.current.getContext("2d");
+    const snowflakes = createSnowflakes();
+
+    gif.on("finished", function (blob) {
+      const url = URL.createObjectURL(blob);
+
+      // 다운로드 링크 생성
+      const downloadLink = document.createElement("a");
+      downloadLink.href = url;
+      downloadLink.download = "snow_effect.gif"; // 다운로드되는 파일의 이름
+      downloadLink.text = "Download GIF"; // 링크 텍스트
+      downloadLink.style.display = "block"; // 링크를 보이게 설정
+      document.body.appendChild(downloadLink); // 링크를 문서에 추가
+    });
+
+    const drawFrame = () => {
+      ctx.clearRect(0, 0, imageSize.width, imageSize.height);
+      ctx.drawImage(image, 0, 0, imageSize.width, imageSize.height);
+      snowflakes.forEach((flake) => {
+        flake.update();
+        flake.draw(ctx);
+      });
+    };
+
+    for (let i = 0; i < 20; i++) {
+      drawFrame();
+      gif.addFrame(canvasRef.current, { copy: true, delay: 200 });
+    }
+
+    gif.render();
+  };
+
+  const createSnowflakes = () => {
+    const flakes = [];
+    for (let i = 0; i < 600; i++) {
+      flakes.push(new Flake(snowflakeSize, snowflakeSpeed, snowflakeColor));
+    }
+    return flakes;
+  };
+
   return (
     <div>
       <input type="file" onChange={handleImageChange} />
@@ -138,6 +189,8 @@ const SnowEffectOnUploadedImage = () => {
             onChange={(e) => setSnowflakeColor(e.target.value)}
           />
         </label>
+        <button onClick={generateGIF}>Generate GIF</button>
+        {/* GIF 생성 버튼 */}
       </div>
     </div>
   );
