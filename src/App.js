05b3060b50e6bdeeef87f039ffd5a89abb3d1ec0
leechangjoon <2018110021@dongguk.edu>
2024-01-11 17:58:32 +0900

    [Refactor] App.js 디자인 코드 분리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import * as S from "./style";
+
 import HomePage from "./pages/home/HomePage";
 import ListPage from "./pages/list/ListPage"; // 가정한 ListPage 경로
 import NavBar from "./pages/layout/Navbar";
+import Footer from "./pages/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/list" element={<ListPage />} />
-      </Routes>
+      <S.Wrapper>
+        <NavBar />
+        <S.ContentWrapper>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/list" element={<ListPage />} />
+          </Routes>
+        </S.ContentWrapper>
+        <Footer />
+      </S.Wrapper>
     </Router>
   );
 }
---
+++ src/style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
