b821ea28f9529b98243df21a19f4ef814826168a
ChoiSangwon <fdsa200@naver.com>
2024-01-13 02:05:19 +0900

    feat : gif, list get요청 및 다운로드 구현

--- src/components/ListCard/GifListCard.jsx
+++ src/components/ListCard/GifListCard.jsx
@@ -1,20 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./style";
 import ImageDownloadPopup from "../Popup/ImageDownloadPopup";
+import AWS from "aws-sdk";
+
+const S3_BUCKET = "snowpickbucket";
+const REGION = "ap-northeast-2";
+
+AWS.config.update({
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_SECRET_KEY,
+});
+
+const s3 = new AWS.S3({
+  region: REGION,
+  params: { Bucket: S3_BUCKET },
+});
 
 const GifListCard = () => {
   const [showDownloadPopup, setShowDownloadPopup] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const BASE_API_URL = process.env.REACT_APP_API;
+
+    async function fetchData() {
+      try {
+        const response = await fetch(BASE_API_URL + "/gif");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const extractFilenameFromUrl = (url) => {
+    const parts = url.split("/");
+    return parts[parts.length - 1];
+  };
 
   const handleClickCard = (image) => {
     setSelectedImage(image); // 이미지 state 업데이트
     setShowDownloadPopup(true); // 팝업 show
   };
 
-  const handleDownload = () => {
-    // 이미지 다운로드 로직
-    // selectedImage를 사용하여 다운로드 할 예정
-    setShowDownloadPopup(false); // 팝업 noShow
+  const downloadFile = (url) => {
+    url =
+      process.env.REACT_APP_S3 + "gif/" + extractFilenameFromUrl(selectedImage);
+
+    fetch(url, { method: "GET" })
+      .then((res) => {
+        return res.blob();
+      })
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+
+        a.href = url;
+        a.download = extractFilenameFromUrl(selectedImage);
+        document.body.appendChild(a);
+        a.click();
+        setTimeout((_) => {
+          window.URL.revokeObjectURL(url);
+        }, 60000);
+        a.remove();
+        console.log();
+        // setOpen(false);
+      })
+      .catch((err) => {
+        console.error("err: ", err);
+      });
   };
 
   const handleCancel = () => {
@@ -24,23 +87,19 @@ const GifListCard = () => {
   return (
     <S.ListContainer>
       <S.GifListContiner>
-        <S.GifCardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.GifImageContainer src="" />
-        </S.GifCardContainer>
-        <S.GifCardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.GifImageContainer src="" />
-        </S.GifCardContainer>
-        <S.GifCardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.GifImageContainer src="" />
-        </S.GifCardContainer>
-        <S.GifCardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.GifImageContainer src="" />
-        </S.GifCardContainer>
+        {data.map((res) => (
+          <S.GifCardContainer onClick={() => handleClickCard(res)} key={res}>
+            <S.GifImageContainer src={res} />
+          </S.GifCardContainer>
+        ))}
       </S.GifListContiner>
 
       {showDownloadPopup && (
         <ImageDownloadPopup
-          onDownload={handleDownload}
+          // onDownload={() => {
+          //   handleDownload(selectedImage);
+          // }}
+          onDownload={downloadFile}
           onCancel={handleCancel}
         />
       )}
--- src/components/ListCard/ListCard.jsx
+++ src/components/ListCard/ListCard.jsx
@@ -1,20 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./style";
 import ImageDownloadPopup from "../Popup/ImageDownloadPopup";
 
 const ListCard = () => {
   const [showDownloadPopup, setShowDownloadPopup] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const BASE_API_URL = process.env.REACT_APP_API;
+
+    async function fetchData() {
+      try {
+        const response = await fetch(BASE_API_URL + "/custom/image");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        setData(result);
+        console.log(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const extractFilenameFromUrl = (url) => {
+    const parts = url.split("/");
+    return parts[parts.length - 1];
+  };
 
   const handleClickCard = (image) => {
     setSelectedImage(image); // 이미지 state 업데이트
     setShowDownloadPopup(true); // 팝업 show
   };
 
-  const handleDownload = () => {
-    // 이미지 다운로드 로직
-    // selectedImage를 사용하여 다운로드 할 예정
-    setShowDownloadPopup(false); // 팝업 noShow
+  const downloadFile = (url) => {
+    console.log(extractFilenameFromUrl(selectedImage));
+    url =
+      process.env.REACT_APP_S3 +
+      "custom/" +
+      extractFilenameFromUrl(selectedImage);
+
+    fetch(url, { method: "GET" })
+      .then((res) => {
+        return res.blob();
+      })
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+
+        a.href = url;
+        a.download = extractFilenameFromUrl(selectedImage);
+        document.body.appendChild(a);
+        a.click();
+        setTimeout((_) => {
+          window.URL.revokeObjectURL(url);
+        }, 60000);
+        a.remove();
+        console.log();
+        // setOpen(false);
+      })
+      .catch((err) => {
+        console.error("err: ", err);
+      });
   };
 
   const handleCancel = () => {
@@ -24,28 +77,15 @@ const ListCard = () => {
   return (
     <S.ListContainer>
       <S.ListRowContainer>
-        <S.CardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.ImageContainer src="" />
-        </S.CardContainer>
-        <S.CardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.ImageContainer src="" />
-        </S.CardContainer>
-        <S.CardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.ImageContainer src="" />
-        </S.CardContainer>
-        <S.CardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.ImageContainer src="" />
-        </S.CardContainer>
-        <S.CardContainer onClick={() => handleClickCard("image1.jpg")}>
-          <S.ImageContainer src="" />
-        </S.CardContainer>
+        {data.map((res) => (
+          <S.CardContainer onClick={() => handleClickCard(res)}>
+            <S.ImageContainer src={res} />
+          </S.CardContainer>
+        ))}
       </S.ListRowContainer>
 
       {showDownloadPopup && (
-        <ImageDownloadPopup
-          onDownload={handleDownload}
-          onCancel={handleCancel}
-        />
+        <ImageDownloadPopup onDownload={downloadFile} onCancel={handleCancel} />
       )}
     </S.ListContainer>
   );
