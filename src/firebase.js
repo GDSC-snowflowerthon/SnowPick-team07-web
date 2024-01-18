7661e1d7956cf6d7eab9e8a7f47309253a8c02dd
ChoiSangwon <fdsa200@naver.com>
2024-01-18 15:00:09 +0900

    feat : firebase 추가

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMIAN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const fstorage = getStorage(firebaseApp);
